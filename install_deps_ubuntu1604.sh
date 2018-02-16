54886441bb3cff5327b19bceede175d8cbf04688
Miguel Raggi <mraggi@gmail.com>
2018-02-15 23:48:41 -0600

    Travis still doesn't work

--- install_deps_ubuntu1604.sh
+++ install_deps_ubuntu1604.sh
@@ -2,16 +2,16 @@
 
 # Update and upgrade first
 sudo apt-get update
-sudo apt-get -y upgrade
+# sudo apt-get -y upgrade
 
 # Basic things
 sudo apt-get -y install build-essential cmake git software-properties-common python-software-properties libboost-dev libgtest-dev wget clang
 
 # Set up gtest
-cd /usr/src/gtest/
-sudo cmake .
-sudo make
-sudo cp *.a /usr/lib
+# cd /usr/src/gtest/
+# sudo cmake .
+# sudo make
+# sudo cp *.a /usr/lib
 
 # Prepare to install SFML
 sudo apt-get -y install libxrandr-dev libopenal-dev libfreetype6 libfreetype6-dev libflac-dev libogg-dev libvorbis-dev libopenal-dev libudev-dev libsndfile-dev libjpeg-dev libglew-dev
@@ -25,6 +25,6 @@ unzip SFML-2.4.2-sources.zip
 cd SFML-2.4.2
 mkdir build
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=/usr
+cmake .. -DBUILD_TESTS=OFF -DCMAKE_INSTALL_PREFIX=/usr
 make
 sudo make install
